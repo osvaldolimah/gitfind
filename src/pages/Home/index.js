42789613febfcdd39a42bbc538efe9abc11d5c71
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-12-13 22:04:56 -0300

    estilizando o projeto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
 import Header  from "../../components/Header";
+import background from "../../assets/background.png"
+import "./style.css"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app"/>
+        <div className="info">teste</div>
+        <div className="user">
+          <input name="usuario" placeholder="username" />
+          <button>Button</button>
+        </div>
+      </div>
     </div>
   );
 }
